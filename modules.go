080b7544b59d960ba118e73debc5a4e53e8fad68
Aleksandr <alex@tresorg.com>
2019-02-05 18:29:35 +0100

    Update modules.go
    multiline support

--- modules.go
+++ modules.go
@@ -4,4 +4,5 @@ import (
   _ "github.com/looplab/logspout-logstash"
   _ "github.com/gliderlabs/logspout/transports/udp"
   _ "github.com/gliderlabs/logspout/transports/tcp"
+  _ "github.com/gliderlabs/logspout/adapters/multiline"
 )
